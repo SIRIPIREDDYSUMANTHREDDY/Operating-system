5607aa10b19f5bd8673b4ed4a119618e6e4aa5cf
SIRIPIREDDYSUMANTHREDDY <sumanthreddy8019@gmail.com>
2025-06-24 11:36:10 +0530

    Update and rename OS EXPERIMENT-5.cpp to OS EXPERIMENT-05.cpp

--- OS EXPERIMENT-05.cpp
+++ OS EXPERIMENT-05.cpp
@@ -1,26 +1,22 @@
 #include <stdio.h>
-
 struct Process {
-    int pid;            // Process ID
-    int burst_time;     // Burst Time
-    int priority;       // Priority (Lower value means higher priority)
-    int waiting_time;   // Waiting Time
-    int turnaround_time;// Turnaround Time
+    int pid;
+    int burst_time;
+    int priority; 
+    int waiting_time;
+    int turnaround_time;
 };
-
 void calculateWaitingTime(struct Process p[], int n) {
     p[0].waiting_time = 0;
     for (int i = 1; i < n; i++) {
         p[i].waiting_time = p[i-1].waiting_time + p[i-1].burst_time;
     }
 }
-
 void calculateTurnaroundTime(struct Process p[], int n) {
     for (int i = 0; i < n; i++) {
         p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
     }
 }
-
 void sortByPriority(struct Process p[], int n) {
     struct Process temp;
     for (int i = 0; i < n-1; i++) {
@@ -33,7 +29,6 @@ void sortByPriority(struct Process p[], int n) {
         }
     }
 }
-
 void printProcesses(struct Process p[], int n) {
     printf("\nPID\tPriority\tBurst Time\tWaiting Time\tTurnaround Time\n");
     for (int i = 0; i < n; i++) {
@@ -41,26 +36,19 @@ void printProcesses(struct Process p[], int n) {
                p[i].pid, p[i].priority, p[i].burst_time, p[i].waiting_time, p[i].turnaround_time);
     }
 }
-
 int main() {
     int n;
-
     printf("Enter number of processes: ");
     scanf("%d", &n);
-
     struct Process p[n];
-
     for (int i = 0; i < n; i++) {
         p[i].pid = i + 1;
         printf("Enter burst time and priority for process %d: ", p[i].pid);
         scanf("%d %d", &p[i].burst_time, &p[i].priority);
     }
-
     sortByPriority(p, n);
     calculateWaitingTime(p, n);
     calculateTurnaroundTime(p, n);
     printProcesses(p, n);
-
     return 0;
 }
-
